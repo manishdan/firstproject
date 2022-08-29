23832b97f900ef6391f3d70d872917599dacc137
manishdangol <manishdangol42@gmail.com>
2022-08-29 11:33:16 +0545

    add list in todo

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -1,30 +1,66 @@
-import React from 'react'
-import'./Todo.css'
+import React, { useState } from 'react'
+import './Todo.css'
 
+const Todo = () => {
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState([
+        'Take a walk.',
+        'Make a presentation.',
+        'Cook food.',
+        'Feed the dog.'
+    ]);
 
-function Todo() {
-  return (
-    <div className='todo'>
-     <div className='todo-logo'>
-        <h3>Todo App</h3>
-     </div>
-     <div className='todo-from'>
-        <form>
-            <input type="text" className="task" name="task"/>
-            <input type="button" className="button" value="Enter"/>
-        </form>
-     </div>
-     <div className=' tode-lists'>
-        <h3>Task list</h3>
-        <ul>
-            <li>take a walk</li>
-            <li> Make a presentation</li>
-            <li>Cook food</li>
-            <li>feed a dog</li>
-        </ul>
-     </div>
-    </div>
-  )
+    console.log(input)
+    console.log(todos)
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     setTodos([...todos, input])
+    //     setInput('')
+    // }
+
+    function handleSubmit() {
+        setTodos([...todos, input])
+        setInput('')  
+    }
+
+    return (
+        <div className='todo'>
+            <div className="todo-logo">
+                <h3>Todo App</h3>
+            </div>
+
+            <div className="todo-form">
+                <form>
+                    <input
+                        type="text"
+                        className="task"
+                        name="input"
+                        value={input}
+                        onChange={ e => setInput(e.target.value)}
+                    />
+                    <input
+                        type="submit"
+                        className="button"
+                        name="button"
+                        value="Add Todo" 
+                        disabled={!input}
+                        onClick={handleSubmit}
+                    />
+                </form>
+            </div>
+
+            <div className="todo-lists">
+                <h3>Task Lists</h3>
+                <ul>
+                    {todos.map((todo) => (
+                        <li>{todo}</li>
+                    ))
+                    }
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Todo
